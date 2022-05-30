ef36a982a91dd85ea011d196e70299d76f0e5ce0
CodeWracker <dev.rodrigofs@gmail.com>
2022-05-30 19:23:57 -0300

    comecei as anotações do metodo de newton

---
+++ Testes_Metodos/02_zeros-de-funcoes/newton/newton.py
@@ -0,0 +1,21 @@
+# E um caso particular do método do Ponto Fixo. ´
+# Ideia: construir φ(x) que satisfaça as condi¸c˜oes do Teorema do Ponto Fixo.
+# Linearizar: substituir f(x) pela reta tangente.
+# Vamos impor que φ'(x∗) = 0. Como φ'(x) deve ser cont´ınua, existe I tal que k = max_x∈I |φ'(x)| < 1.
+# De forma geral, x = φ(x) equivalente a f(x) = 0 é dada por:
+# f(x) = 0 ⇒ A(x)f(x) = x − x ⇒ x = x + A(x)f(x) = φ(x), sendo A(x) contínua e A(x∗) �= 0.
+# Vamos determinar A(x) de forma que φ'(x∗) = 0
+# Temos que:
+# φ'(x) = 1 + A(x)f'(x) + A'(x)f(x)
+# Para x = x*
+# φ'(x∗) = 1 + A(x∗)f'(x∗) + A'(x∗)f(x∗) = 0,
+# j´a que queremos φ'(x∗) = 0 e supondo f'(x∗) �= 0. Assim,
+# φ'(x∗) = 1 + A(x∗)f'(x∗) = 0 ⇒ A(x∗) = − 1/f'(x∗)
+# Portanto escolhemos,
+# A(x) = − 1/f'(x)
+# A função de iteração do método de Newton é
+# φ(x) = x − f(x)/f'(x) ⇒ xn+1 = xn − f(xn)/f'(xn) , para i = 0, 1, ...
+# O método de Newton converge mais rápido que o método do Ponto Fixo.
+# Pode-se mostrar que a convergência do método de Newton é quadrática se f'(x∗) != 0 e linear, caso contrário.
+# A convergência é linear quando |x_n − x∗| ≤ M|x_n−1 − x∗|
+# A convergência é quadrática quando |x_n − x∗| ≤ M|x_n−1 − x∗|^2.
